a7d175f44c9ed58e46249ab8263e0a121fc7d182
jeffreyally <jeffreyally@gmail.com>
2022-02-19 20:30:57 +0000

    updating starwars project

--- src/js/views/detailsview.js
+++ src/js/views/detailsview.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 export const DetailsView = (props) => {
- console.log(props.location.state)
+  let obj = props.location.state
+ console.log(obj)
   return(
 <>
-    <h1>
-      test
-    </h1>
+<div class="card m-1" >
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={obj.imgURL} className="fit" alt="..."  class="w-100"/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
     </>
   )
  
