543a2d5cf7a92aa1eb738c68884cdcc62a6ed326
jeffreyally <jeffreyally@gmail.com>
2022-02-23 03:18:02 +0000

    working on details view

--- src/js/views/detailsview.js
+++ src/js/views/detailsview.js
@@ -8,24 +8,19 @@ export const DetailsView = (props) => {
   return (
     <>
       <div className="card m-1">
-        <div className="row g-0">
-          <div className="col-md-4">
+        <div className="row g-0 d-flex">
+          <div className="col-md-4 d-flex">
             <img src={obj.imgURL} className="fit" alt="..." className="w-100" />
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title">{obj.name}</h5>
+              <h5 className="card-title" style={{textAlign:'center'}}>{obj.name}</h5>
               <p className="card-text">
-                <span>{`${obj.prop1}: ${obj.value1}`}</span>
-                <br></br>
-                <span>
-                  {`${obj.prop2}: ${obj.value2}`}
-                  <br></br>
-                </span>
-                {`${obj.prop3}: ${obj.value3}`}
+              
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               </p>
               <Link to="/">
-              <button className="btn btn-primary">Home</button>
+              <button className="btn btn-primary" style={{float:'right' ,marginBottom:'10px' ,marginTop:'80px'}}>Home</button>
               </Link>
             </div>
           </div>
@@ -34,3 +29,12 @@ export const DetailsView = (props) => {
     </>
   );
 };
+
+
+// let threefacts = <span>{`${obj.prop1}: ${obj.value1}`}</span>
+// <br></br>
+// <span>
+//   {`${obj.prop2}: ${obj.value2}`}
+//   <br></br>
+// </span>
+// {`${obj.prop3}: ${obj.value3}`}
