165e929d24a617a6b0c7826643743d23ec4621b1
jeffreyally <jeffreyally@gmail.com>
2022-02-23 01:24:48 +0000

    project

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-
+let starwarslogo ='https://www.cultofwhatever.com/wp-content/uploads/2015/09/star-wars-retro-logo-2.png'
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
 	return (
+    <>
+    
 		<div className="dropdown">
+    <img className="navbarlogotopleft" src={starwarslogo}/>
       <span className="spanAroundFavoritesList">
   <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
     Favorites List
@@ -22,6 +25,6 @@ export const Navbar = () => {
   <br></br>
   
 </div>
-
+</>
 	);
 };
