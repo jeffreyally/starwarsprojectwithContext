afeb493eecf22fad9f001eca0ea6c9a5f6ad1c4a
jeffreyally <jeffreyally@gmail.com>
2022-02-23 02:02:09 +0000

    logo takes us home

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,30 +2,39 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-let starwarslogo ='https://www.cultofwhatever.com/wp-content/uploads/2015/09/star-wars-retro-logo-2.png'
+let starwarslogo =
+  "https://www.cultofwhatever.com/wp-content/uploads/2015/09/star-wars-retro-logo-2.png";
 export const Navbar = () => {
-	const { store, actions } = useContext(Context);
-	return (
+  const { store, actions } = useContext(Context);
+  return (
     <>
-    
-		<div className="dropdown d-flex justify-content-between">
-      <div><img className="navbarlogotopleft" src={starwarslogo}/></div>
-    
-      <div className="divAroundFavoritesList">
-  <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    Favorites List
-  </button>
-  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-    {store.favorites.map((fav,index)=>{
-		return <li>{fav.name}</li>
-	})}
-  </ul>
-  </div>
-  <br></br>
-  <br></br>
-  <br></br>
-  
-</div>
-</>
-	);
+      <div className="dropdown d-flex justify-content-between">
+        <Link to="/">
+          <div>
+            <img className="navbarlogotopleft" src={starwarslogo} />
+          </div>
+        </Link>
+
+        <div className="divAroundFavoritesList">
+          <button
+            className="btn btn-primary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Favorites List
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            {store.favorites.map((fav, index) => {
+              return <li>{fav.name}</li>;
+            })}
+          </ul>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+      </div>
+    </>
+  );
 };
